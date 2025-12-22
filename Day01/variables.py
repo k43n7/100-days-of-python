1464aa27aa41d1ea0163496ce0bd1991e0ef2aa4
Bjorn <142839691+k43n7@users.noreply.github.com>
2025-12-21 22:58:56 -0600

    Day 1: Variables

---
+++ Day01/variables.py
@@ -0,0 +1,18 @@
+# name = "Ziyad"
+# print(name)
+
+
+# name = "Ziyad"
+# print(name)
+
+
+# print(len(input("What is your name?")))
+
+# username = (input("what is your name?"))
+# length = len(username)
+# print(length)
+
+
+hackername = (input("sir, what is your username? "))
+length1 = len(hackername)
+print(length1)
