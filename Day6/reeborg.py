5450bd62f7cd67a4bc5b099aa1ecef702335c88c
Ziyad <142839691+k43n7@users.noreply.github.com>
2025-12-30 17:09:48 -0600

    Added New codes to the reeborg race game

--- Day6/reeborg.py
+++ Day6/reeborg.py
@@ -8,7 +8,6 @@ def turn_right():
     turn_left()
     
 def jump():
-    move()
     turn_left()
     move()
     turn_right()
@@ -17,10 +16,58 @@ def jump():
     move()
     turn_left()
 
-for steps in range(6):
+def move_up():
+    turn_left()
+    move()
+    turn_right()
+
+    
+#What you need to know
+# The functions move() and turn_left().
+# The conditions front_is_clear() 
+    # or wall_in_front(), at_goal(), 
+        # and their negation.
+# How to use a while loop and an if statement.
+
+
+
+
+"""
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+"""
+
+    
+   
+
+"""
+number_of_hurtles = 6
+position = True
+while number_of_hurtles > 0:
+    if position != at_goal():
+        jump()
+        number_of_hurtles -= 1
+        print(number_of_hurtles)
+    break;
+
+# what we are supposed to do:
+while not at_goal():
     jump()
-
-
+"""
+    
+    
+"""
+number_of_hurtles = 6
+while number_of_hurtles > 0:
+    jump()
+    number_of_hurtles -= 1
+    print(number_of_hurtles)
+"""
+    
+    
 
 # Draw Square
 """
