0069b09005d1c23f509648fff76a144d4d74c925
Ziyad <142839691+k43n7@users.noreply.github.com>
2025-12-30 14:50:52 -0600

    Day 6 - we played reeborg

---
+++ Day6/reeborg.py
@@ -0,0 +1,54 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for steps in range(6):
+    jump()
+
+
+
+# Draw Square
+"""
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+"""
+
+
+# My play   
+"""
+turn_left()
+move()
+turn_right()
+move()
+turn_around()
+turn_left()
+move()
+turn_left()
+move()
+"""
+
+
+
+
+
